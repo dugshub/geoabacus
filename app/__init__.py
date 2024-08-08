2c974cae844700e4b051b5a6ff3e3466ab3b9c7f
dug <dug.mcfarlane@icloud.com>
2024-08-08 10:36:26 -0400

    db setup and environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app import models
 @app.route('/')
 @app.route('/home')
 def home():
--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+print(f'sqlite:///{basedir}' + f'{os.environ.get('SQLITE_DATABASE_PATH')}')
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{basedir}' + f'{os.environ.get('SQLITE_DATABASE_PATH')}' or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
