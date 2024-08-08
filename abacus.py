ddc34288d3371e4eb0a87a8e2466da79d4f85b3b
dug <dug.mcfarlane@icloud.com>
2024-08-08 17:06:45 -0400

    finnaly set up connexion - must use uvicorn to run

--- abacus.py
+++ abacus.py
@@ -1,6 +1,13 @@
-from app import app, db
+from app import app, db, connex_app
 from app.models import Dimension
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Dimension': Dimension}
+
+# app = create_app()
+# app = app.app
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'Dimension': Dimension}
+
+if __name__ == '__main__':
+    connex_app.run(port=5001)
 
--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
+from flask import render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from connexion import FlaskApp
 
 from config import Config
 
-app = Flask(__name__)
+def create_app():
+    app = FlaskApp(__name__)
+    app.add_api("swagger.yml", base_path="/api")
+    return app
+
+connex_app = create_app()
+app = connex_app.app
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import models
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     user = {'username': 'Doug'}
-    return render_template('home.html',title='Home',user=user)
+    return render_template('home.html', title='Home', user=user)
