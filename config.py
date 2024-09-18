55ecdb20d1c2216cc016272671d22cb0c0cba5c5
dugshub <163685195+dugshub@users.noreply.github.com>
2024-09-17 20:46:31 -0400

    complete ground-up rebuild that incorporates geoalchemy for shapefile storing

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 print(f'sqlite:///{basedir}' + f"{os.environ.get('SQLITE_DATABASE_PATH')}")
 
+load_dotenv(dotenv_path=f"{basedir}/.env")
 class Config:
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{basedir}' + f"{os.environ.get('SQLITE_DATABASE_PATH')}" or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
