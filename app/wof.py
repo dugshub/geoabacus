a6c1048b1b33fb4291be0f0569866b4abd96e482
dugshub <163685195+dugshub@users.noreply.github.com>
2024-09-21 12:47:15 -0400

    updated WoF interface to pull related placetypes supplied by user for a list of IDs supplied by user

--- app/wof.py
+++ app/wof.py
@@ -5,6 +5,8 @@ from sqlalchemy import text
 
 wofdb = os.environ.get("WOF_SQLITE_PATH")
 engine = sa.create_engine(f'sqlite:///{wofdb}')
+
+
 def get_geojson(query=None):
     """
     Returns a list of sqlalchemy.engine.row.RowMapping items of WhosOnFirst geojson blobs.
@@ -47,6 +49,7 @@ def get_geojson(query=None):
         )
         result = connection.execute(query)
         return [row._mapping for row in result.fetchall()]
+
 
 def from_ids(wof_ids=[None]):
     """
@@ -98,7 +101,7 @@ def from_ids(wof_ids=[None]):
         return [row._mapping for row in result.fetchall()]
 
 
-def get_related_placetypes(wof_ids=[None],placetypes=['neighbourhood']):
+def get_related_placetypes(wof_ids=(101735835, ''), placetypes=('neighbourhood', '')):
     """
     Returns a list of sqlalchemy.engine.row.RowMapping items of WhosOnFirst geojson blobs from a list of
     WhosOnFirst ids. This function will pull all associated placetypes (
@@ -110,43 +113,58 @@ def get_related_placetypes(wof_ids=[None],placetypes=['neighbourhood']):
         list: Objects of WhosOnFirst data. Can be indexed as a dictionary or through attribute notation.
     """
 
-    wof_ids = wof_ids or []
     with engine.connect() as connection:
         query = text(
             f"""
-                with wof_ids as (
-                    select * from spr
-                        where
-                            is_current = 1 and 
-                            id in {wof_ids}
-                ),
+                with
+                    dims as (
+                        select * from spr
+                    ),
+                
+                    parents as (
+                    select *
+                    from ancestors
+                    where id in {wof_ids} and
+                          ancestor_placetype in {placetypes} and
+                          ancestor_id != id
+                    ),
+                
+                    children as (
+                    select ancestors.id as child_id,
+                           dims.placetype as child_placetype,
+                           ancestors.ancestor_id,
+                           ancestors.ancestor_placetype
+                    from ancestors
+                        left join dims on dims.id = ancestors.id
+                    where ancestor_id in {wof_ids} and
+                          is_current and
+                          placetype in {placetypes} and
+                          child_id != ancestors.ancestor_id
+                    ),
+           
+                    parents_and_children as (
+                        select ancestor_id as wof_id
+                        from parents
+                
+                        union all
+                
+                        select child_id as wof_id
+                        from
+                            children
+                    ),
+                
+                    shapes as (
+                        select
+                            geojson.body as geojson_data
+                        from geojson
+                        where id in (select * from parents_and_children) and
+                              not is_alt
+                    )
+                    
+                    select distinct * from shapes
 
-
-                hierarchy as (
-                    select * from ancestors 
-                    left join wof_ids on wof_ids.id = ancestors.id
-                ),
-
-                shapes as (
-                    select * from geojson
-                    left join hierarchy on shapes.id = hierarchy.id
-                )
-
-                select  
-                    wof_ids.id,
-                    parent_id as parent,
-                    name,
-                    placetype,
-                    country,
-                    body as geojson,
-                    is_current,
-                    is_alt
-                from wof_ids
-                left join shapes on wof_ids.id = shapes.id
-                where not shapes.is_alt 
              """
         )
         result = connection.execute(query)
         connection.close()
         return [row._mapping for row in result.fetchall()]
-
